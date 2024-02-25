984caf44617c94d4c52b85624ccdc4e8ed6cc147
Neo <neo@mail.com>
2024-02-25 00:05:48 -0500

    Worked on layout

--- VigilantNetworkMonitor/RootForm.Designer.cs
+++ VigilantNetworkMonitor/RootForm.Designer.cs
@@ -40,23 +40,26 @@
             applyFilterButton = new Button();
             statusStrip1 = new StatusStrip();
             toolStripErrorLabel = new ToolStripStatusLabel();
+            tableLayoutPanel1 = new TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)packetDataGridView1).BeginInit();
             menuStrip.SuspendLayout();
             statusStrip1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // packetDataGridView1
             // 
             packetDataGridView1.AllowUserToAddRows = false;
             packetDataGridView1.AllowUserToDeleteRows = false;
+            packetDataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             packetDataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             packetDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             packetDataGridView1.Columns.AddRange(new DataGridViewColumn[] { sourceIp, sourcePort, destinationIP, destinationPort, protocol });
             packetDataGridView1.EditMode = DataGridViewEditMode.EditProgrammatically;
-            packetDataGridView1.Location = new Point(12, 56);
+            packetDataGridView1.Location = new Point(12, 62);
             packetDataGridView1.Name = "packetDataGridView1";
             packetDataGridView1.ReadOnly = true;
-            packetDataGridView1.Size = new Size(776, 382);
+            packetDataGridView1.Size = new Size(778, 357);
             packetDataGridView1.TabIndex = 0;
             // 
             // sourceIp
@@ -91,9 +94,10 @@
             // 
             // sniffButton
             // 
-            sniffButton.Location = new Point(733, 27);
+            sniffButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            sniffButton.Location = new Point(664, 3);
             sniffButton.Name = "sniffButton";
-            sniffButton.Size = new Size(55, 23);
+            sniffButton.Size = new Size(69, 23);
             sniffButton.TabIndex = 2;
             sniffButton.Text = "Sniff";
             sniffButton.UseVisualStyleBackColor = true;
@@ -104,7 +108,7 @@
             menuStrip.Items.AddRange(new ToolStripItem[] { file, tools });
             menuStrip.Location = new Point(0, 0);
             menuStrip.Name = "menuStrip";
-            menuStrip.Size = new Size(800, 24);
+            menuStrip.Size = new Size(802, 24);
             menuStrip.TabIndex = 3;
             menuStrip.Text = "menuStrip1";
             // 
@@ -140,25 +144,29 @@
             // 
             // filterTextBox
             // 
-            filterTextBox.Location = new Point(51, 27);
+            filterTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            filterTextBox.Location = new Point(3, 3);
             filterTextBox.Name = "filterTextBox";
-            filterTextBox.Size = new Size(615, 23);
+            filterTextBox.Size = new Size(582, 23);
             filterTextBox.TabIndex = 4;
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label1.AutoSize = true;
-            label1.Location = new Point(12, 31);
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 32);
             label1.Name = "label1";
-            label1.Size = new Size(33, 15);
+            label1.Size = new Size(36, 17);
             label1.TabIndex = 5;
             label1.Text = "Filter";
             // 
             // applyFilterButton
             // 
-            applyFilterButton.Location = new Point(672, 27);
+            applyFilterButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            applyFilterButton.Location = new Point(591, 3);
             applyFilterButton.Name = "applyFilterButton";
-            applyFilterButton.Size = new Size(55, 23);
+            applyFilterButton.Size = new Size(67, 23);
             applyFilterButton.TabIndex = 6;
             applyFilterButton.Text = "Apply";
             applyFilterButton.UseVisualStyleBackColor = true;
@@ -167,9 +175,9 @@
             // statusStrip1
             // 
             statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripErrorLabel });
-            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Location = new Point(0, 409);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.Size = new Size(802, 22);
             statusStrip1.TabIndex = 7;
             statusStrip1.Text = "statusStrip1";
             // 
@@ -179,16 +187,33 @@
             toolStripErrorLabel.Name = "toolStripErrorLabel";
             toolStripErrorLabel.Size = new Size(0, 17);
             // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(applyFilterButton, 1, 0);
+            tableLayoutPanel1.Controls.Add(filterTextBox, 0, 0);
+            tableLayoutPanel1.Controls.Add(sniffButton, 2, 0);
+            tableLayoutPanel1.Location = new Point(54, 27);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(736, 29);
+            tableLayoutPanel1.TabIndex = 8;
+            // 
             // RootForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(statusStrip1);
-            Controls.Add(applyFilterButton);
+            ClientSize = new Size(802, 431);
             Controls.Add(label1);
-            Controls.Add(filterTextBox);
-            Controls.Add(sniffButton);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(statusStrip1);
             Controls.Add(packetDataGridView1);
             Controls.Add(menuStrip);
             MainMenuStrip = menuStrip;
@@ -200,6 +225,8 @@
             menuStrip.PerformLayout();
             statusStrip1.ResumeLayout(false);
             statusStrip1.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -223,5 +250,6 @@
         private DataGridViewTextBoxColumn protocol;
         private StatusStrip statusStrip1;
         private ToolStripStatusLabel toolStripErrorLabel;
+        private TableLayoutPanel tableLayoutPanel1;
     }
 }
